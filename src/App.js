4d415a78b9630f8c50b40a37fc5993200ab10f91
Alex Araujo <alex.fate2000@gmail.com>
2022-06-26 14:28:22 -0300

    Minor changes. Add 80 port exposure

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Alex Araujo
+          Alex Araujo Testando na cloud AWS.
         </p>
         <a
           className="App-link"
